44d2f23ce6413200ba66d5d18cfc353e6652558d
afg <afg984@gmail.com>
2015-09-22 00:56:58 +0800

    copied and pasted

--- ditroboticstw/settings/openshift.py
+++ ditroboticstw/settings/openshift.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nthucourses',
+        'NAME': 'ditrobotics',
         'USER': os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME'],
         'PASSWORD': os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD'],
     }
